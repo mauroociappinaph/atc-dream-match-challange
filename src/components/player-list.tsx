e550026adcd7e160dcc2970c2fa9e7c092ce51d5
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2024-08-04 17:34:58 -0300

    feat: Agregar spinner de carga en PlayerList component

--- src/components/player-list.tsx
+++ src/components/player-list.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect, useCallback } from "react";
+import React, { useEffect, useCallback, useState } from "react";
 import { debounce } from "lodash";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent } from "@/components/ui/card";
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 import playersApi from "@/app/api/playersApi";
 import { usePlayerListStore } from "@/store/store";
 import { Player } from "@/types/index";
+import LoadingSpinner from "./ui/loadingspinner";
 
 export default function PlayerList() {
   const {
@@ -24,17 +25,23 @@ export default function PlayerList() {
   } = usePlayerListStore();
   const playersPerPage = 6;
 
+  const [isLoading, setIsLoading] = useState(false);
+
   const debouncedFetchPlayers = useCallback(
     debounce(async (term) => {
       if (term.trim() === "") {
         setPlayers([]);
+        setIsLoading(false);
         return;
       }
+      setIsLoading(true);
       try {
         const playersData = await playersApi.getPlayers(term);
         setPlayers(playersData || []);
       } catch (error) {
         console.error(error);
+      } finally {
+        setIsLoading(false);
       }
     }, 1000), // Cambiado a 1000 ms
     [setPlayers]
@@ -78,58 +85,64 @@ export default function PlayerList() {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center p-4">
+    <div className="flex flex-col justify-center items-center  p-4 ">
       <Input
         type="text"
         placeholder="Buscar jugador"
         value={searchTerm || ""}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      {players.length > 0 && (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-          {currentPlayers.map((player) => {
-            if (!player) {
-              return null;
-            }
-            const { player_id, player_name, player_type, player_image } =
-              player;
-            return (
-              <React.Fragment key={player_id}>
-                <Card className="player-card">
-                  <CardContent>
-                    <img
-                      src={player_image}
-                      alt={player_name}
-                      className="player-image"
-                    />
-                    <h2 className="player-name">{player_name}</h2>
-                    <p className="player-position">{player_type}</p>
-                    <Button
-                      onClick={() => handlePlayerSelect(player)}
-                      className="player-button"
-                    >
-                      {selectedPlayers.includes(player)
-                        ? "Deseleccionar"
-                        : "Seleccionar"}
-                    </Button>
-                  </CardContent>
-                </Card>
-              </React.Fragment>
-            );
-          })}
-        </div>
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          {players.length > 0 && (
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 ">
+              {currentPlayers.map((player) => {
+                if (!player) {
+                  return null;
+                }
+                const { player_id, player_name, player_type, player_image } =
+                  player;
+                return (
+                  <React.Fragment key={player_id}>
+                    <Card className="player-card">
+                      <CardContent>
+                        <img
+                          src={player_image}
+                          alt={player_name}
+                          className="player-image"
+                        />
+                        <h2 className="player-name">{player_name}</h2>
+                        <p className="player-position">{player_type}</p>
+                        <Button
+                          onClick={() => handlePlayerSelect(player)}
+                          className="player-button"
+                        >
+                          {selectedPlayers.includes(player)
+                            ? "Deseleccionar"
+                            : "Seleccionar"}
+                        </Button>
+                      </CardContent>
+                    </Card>
+                  </React.Fragment>
+                );
+              })}
+            </div>
+          )}
+          <div className="flex justify-center items-center mt-4">
+            <Button onClick={handlePreviousPage} disabled={currentPage === 1}>
+              Anterior
+            </Button>
+            <Button
+              onClick={handleNextPage}
+              disabled={currentPage === totalPages || totalPages === 0}
+            >
+              Siguiente
+            </Button>
+          </div>
+        </>
       )}
-      <div className="flex justify-center items-center mt-4">
-        <Button onClick={handlePreviousPage} disabled={currentPage === 1}>
-          Anterior
-        </Button>
-        <Button
-          onClick={handleNextPage}
-          disabled={currentPage === totalPages || totalPages === 0}
-        >
-          Siguiente
-        </Button>
-      </div>
     </div>
   );
 }
