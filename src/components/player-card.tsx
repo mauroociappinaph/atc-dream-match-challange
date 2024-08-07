d7917ece71fc687e23131eb415497a3f855f1e5d
MAURO CIAPPINA <100035249+mauroociappinaph@users.noreply.github.com>
2024-08-07 16:44:32 -0300

    feat: Remove player image from PlayerCard component

--- src/components/player-card.tsx
+++ src/components/player-card.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { PlayerCardProps } from "@/types/index";
-import Image from "next/image";
 
 const PlayerCard: React.FC<PlayerCardProps> = ({
   player,
@@ -14,13 +13,6 @@ const PlayerCard: React.FC<PlayerCardProps> = ({
   return (
     <Card className="player-card">
       <CardContent>
-        <Image
-          src={player.player_image}
-          alt={player.player_name}
-          className="player-image"
-          width={150} // Ajusta el valor según tus necesidades
-          height={150} // Ajusta el valor según tus necesidades
-        />
         <h2 className="player-name">{player.player_name}</h2>
         <p className="player-position">{player.player_type}</p>
         <Button onClick={() => onSelect(player)} className="player-button">
